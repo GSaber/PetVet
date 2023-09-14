489992e7c6a5d40a5784452573d0de8328aa2cae
GSaber <saber.guenaoui@gmail.com>
2023-09-14 17:52:26 +0100

    add profile & home pages

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,28 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import Header from "../components/header";
-
+import "../assets/css/home.scss";
 const Home = () => {
   const { currentUser } = useContext(AuthContext);
   return (
     <>
       <Header />
+      <div className="homepage">
+        <div className="container">
+          <div>
+            <h1>
+              Welcome {currentUser?.displayName} Prenez rapidement un
+              rendez-vous avec votre médecin !
+            </h1>
+            <p>
+              Sélectionnez votre médecin, choisissez la date et l'heure de votre
+              rdv et recevez votre sms/mail de confirmation. C’est aussi simple
+              que ça !
+            </p>
+          </div>
+          <img src={require("../assets/img/imghome.jpg")} />
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "../assets/css/profile.scss";
+import { AuthContext } from "../context/AuthContext";
+import ProfileSetting from "../components/profilesettings";
+import Header from "../components/header";
+import ChangePwd from "../components/changepwd";
 const Profile = () => {
+  const [windowContent, setWindowContent] = useState("ProfileSetting");
+  const { currentUser } = useContext(AuthContext);
+
   return (
-    <div className="profile">
-      <div className="profile__container">container</div>
-      <div className="profile__content">content</div>
-    </div>
+    <>
+      <Header />
+      <div className="profile">
+        <div className="userInfo">
+          <img src={currentUser?.photoURL} alt="/" />
+          <span>{currentUser?.displayName}</span>
+          <span>{currentUser?.email}</span>
+
+          <p onClick={() => setWindowContent("ProfileSetting")}>
+            Profile Setting
+          </p>
+          <p onClick={() => setWindowContent("ChangePwd")}>Change Password</p>
+        </div>
+        <div className="profile_content">
+          {windowContent === "ProfileSetting" && <ProfileSetting />}
+          {windowContent === "ChangePwd" && <ChangePwd />}
+        </div>
+      </div>
+    </>
   );
 };
 
