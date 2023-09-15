2d6ef61d6d066d72176583f1fa284d3c44dc0602
GSaber <saber.guenaoui@gmail.com>
2023-09-15 17:57:13 +0100

    no major change

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="profile" element={<Profile />} />
